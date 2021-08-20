79533d36970b811b37c9b88a6453a20879807357
dev <rogerio.pereira@ifto.edu.br>
2021-08-20 08:39:44 -0300

    model

--- pwanime/urls.py
+++ pwanime/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pwanimeapp.views import base, index, teste
+from pwanimeapp.views import  index, teste
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('base/', base ,name='base'),
+    #path('base/', base ,name='base'),
     path('index/', index ,name='index'),
     path('teste/', teste ,name='teste'),
 ]
--- pwanimeapp/models.py
+++ pwanimeapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Lancamento(models.Model):
+	nome_lancamento = models.CharField(max_length=200)
+	link_lancamento = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.nome_lancamento
+
